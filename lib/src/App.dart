4ef215b9a980e75bdd0103d293421a4283eaf513
Reddy Tintaya <aaabeeelooon@gmail.com>
2020-04-15 00:38:31 -0400

    folder imagenes added, route implemented

--- lib/src/App.dart
+++ lib/src/App.dart
@@ -1,11 +1,18 @@
 import 'package:emi_covid/src/pages/HomePage.dart';
+import 'package:emi_covid/src/pages/QuestionsPage.dart';
 import 'package:flutter/material.dart';
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      initialRoute: "/",
+      routes: {
+        "/": (context) => HomePage(),
+        "/questions": (context) => QuestionsPage(),
+
+      },
     );
   }
 }
---
+++ lib/src/pages/QuestionsPage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class QuestionsPage extends StatelessWidget {
+  String title = "TEST DE COVID-19 EMI";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("hola questions")),
+    );
+  }
+}
